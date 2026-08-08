97e2bf5be1978ca9e86740490bbd386a67c3849b
AminTurki <amin.turki@enis.tn>
2026-08-08 19:36:56 +0100

    changement numero

--- src/data/sparky.ts
+++ src/data/sparky.ts
@@ -3,7 +3,7 @@ export const ASKED_FOR_HUMAN_RESPONSE = "ASKED FOR HUMAN";
 export const companyInfo = {
   companyName: "Sparky",
   location: "Sakiet Ezzit, Sfax, Tunisie",
-  phoneNumber: "+216 98 624 731",
+  phoneNumber: "+216 44 500 445",
   email: "sparkysolutionsphotovoltaique@gmail.com",
   website: null,
 };
